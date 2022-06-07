1e79a7076f862d494c3665e77a6eeb85bbd7f131
Adriana McGee <adriana@Adrianas-MBP.lan>
2022-06-07 16:45:24 -0400

    commit tree bd8a7c58027c4a5351b80143c75e3091a3f95493

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+   return `This Saturday, I want to ${activity}!`
+    
+}
+saturdayFun();
+
+function mondayWork(defaultActivity = 'go to the office'){
+    return `This Monday, I will ${defaultActivity}.`
+}
+mondayWork();
+
+function wrapAdjective(){
+    
+}
